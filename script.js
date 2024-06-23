5e85bf9e45b696e1b475c6a6afae243e483b2f1b
SukiMass <sureshkrishna.avi@hotmail.com>
2024-06-23 19:15:37 +0530

    UPDATE - dark theme added

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
+
+function themeFunction(theme){
+    var element =  document.body;
+    element.classList.toggle("light-mode")
+
+    if(!theme.className.includes("bxs-moon")){
+        theme.classList.toggle("bxs-moon")
+        theme.classList.remove("bxs-sun")
+    }
+    else{
+        theme.classList.add("bxs-sun")
+        theme.classList.remove("bxs-moon")
+    }
+}
